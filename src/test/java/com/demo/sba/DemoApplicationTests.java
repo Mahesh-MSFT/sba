427391095fa92a04ccdf7e6dbcc398abddd6e256
Mahesh Kshirsagar <maksh@microsoft.com>
2020-06-26 17:43:34 +0100

    first commit

---
+++ src/test/java/com/demo/sba/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.sba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
